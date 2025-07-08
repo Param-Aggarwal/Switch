33309a6d9e171a0419873172065b600f8fb66dfb
Param-Aggarwal <paramg420@gmail.com>
2025-07-08 23:25:42 +0530

    Used useRef instead of useState + deletion logic corrected

--- TODO App/TodoApp/src/App.jsx
+++ TODO App/TodoApp/src/App.jsx
@@ -2,39 +2,36 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 function App() {
-  // const todo = useRef();
-  const [todo, setTodo] = useState('');
+  const inputRef = useRef("");
+  // const [todo, setTodo] = useState('');
   const [todoList, setTodoList] = useState([]);
 
-  // console.log("setTodoList is ------> ",setTodoList)
-
   const addTodo = () => {
-    console.log("inside addTodo function");
-    console.log("todo is ---------> ", todo)
+    const todo = inputRef.current.value.trim();
+    console.log("todo is --> ",todo)
+    if (todo === "") return;
     const newTodoList = [...todoList, todo];
     setTodoList(newTodoList);
-    setTodo('');
-    console.log("TodoList after insertion is -------> ", newTodoList)
+    inputRef.current.value= "";
+    // setTodo('');
   }
   const deleteTodo = (index) => {
-    console.log("inside delete Todo function");
-    const newTodoList = todoList.filter(todo => todo !== todoList[index])
-    console.log(" after deltion ------------> ",newTodoList)
+    const newTodoList = todoList.filter((todo,i) => i !== index)
     setTodoList(newTodoList)
   }
 
   return (
     <>
-      <div className="todoInput">
+      <div className="todoinputRef">
         <h1>Todo App</h1>
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button onClick={addTodo}> Click Me  </button >
+        <input type="text" ref={inputRef} placeholder="Enter todo here" />  
+        <button onClick={addTodo}> Click to add! </button >
       </div>
       <ul className="todoList">
-        {todoList.map((todo, index) => {
+        {todoList.map((todo,index) => {
           return (
             <li key={index}> {todo}
             <button onClick={() => deleteTodo(index)}> Delete </button>
