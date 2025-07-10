647231d9042c14cc4c84c357962bdfe56c3dffb2
Param-Aggarwal <paramg420@gmail.com>
2025-07-11 00:35:49 +0530

    useId Dos and Donts + form implementation in chatgpt

---
+++ ChatGpt/chatgpt/src/App.jsx
@@ -0,0 +1,47 @@
+import { useId,useRef,useState } from 'react'
+
+function App() {
+  const nameId = useId();
+  const ageId = useId();
+  const contactId = useId();
+  const emailId = useId();
+
+  const name = useRef();
+  const age = useRef();
+  const contact = useRef();
+  // const email = useRef();
+  const [email,setEmail] = useState("");
+  
+  return (
+    <>
+      <h1>Hello World</h1>
+      <label htmlFor={nameId}>Name: </label>
+      <input id={nameId} type="text" ref={name} /> 
+      
+      <label htmlFor={ageId}>Age: </label>
+      <input id={ageId} type="text" ref={age} />
+      
+      <label htmlFor={contactId}>Number: </label>
+      <input id={contactId} type="number" ref={contact} />
+      
+      <label htmlFor={emailId}>Email: </label>
+      <input id={emailId} type="text" value={email}
+        onChange={(e) => {
+        setEmail(e.target.value)
+      }} 
+      />
+      {/* <input id={emailId} type="text" ref={email} /> */}
+
+      <button onClick={() => {
+        console.log("Button Clicked!!")
+        console.log("----------> NAME ",name)
+        console.log("----------> AGE ",age)
+        console.log("----------> CONTACT ",contact)
+        console.log("------------>EMAIL ",emailId);
+      }}>Click Me</button>
+      
+    </>
+  )
+}
+
+export default App
