dba4786ebb25b80c1011c9a6dd0a181001f67a74
Param-Aggarwal <paramg420@gmail.com>
2025-11-12 06:19:30 +0530

    added routing logic

--- playground/playground/src/NestedRoute.jsx
+++ playground/playground/src/NestedRoute.jsx
@@ -4,7 +4,9 @@ import { useOutletContext } from "react-router-dom";
 const Route1 = () => {
     const info  = useOutletContext();
     return <>
-        <h3>This is nested route {(console.log(`context from parent is ${info}`), <small style={{fontSize: "11px"}}> ({info})</small>)}</h3>
+        <h3>This is nested route {(console.log(`context from parent is ${info}`),
+            <small style={{ fontSize: "11px" }}> ({info})</small>)}
+        </h3>
         
     </> 
 }
